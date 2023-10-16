c5640bdad12f2ee0cf63792e5ef6a73b1ef80ab5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-16 13:38:54 -0300

    projetofesta

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+//modules
+
+    const express = require("express");
+    const mongoose = require("mongoose");
+    const bodyParser = require("body-parser");
+    const cors = require("cors");
+
+//routes
+const authRouter = require("./routes/authRoutes");
+
+//middlewares - travas do meio de projeto e acesso
+//config
+
+    const dbName = "partytime";
+    const port = 3000;
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.static("public"));
+
+//atrelar as rotas ao express
+    app.use("/api/auth", authRouter);
+    app.get("/", (req, res)=> {
+        //primeira rota de teste
+    res.json({message : "Rota de teste será trocada!!!"});
+    });
+
+//conexão mongodb
+
+    mongoose.connect(
+        `mongodb://127.0.0.1:27017/${dbName}`, {
+        useNewUrlParser : true,
+        UseUnifiedTopology : true,
+        serverSelectionTimeoutMS : 20000
+    })
+
+//escutando a porta
+    app.listen(port, ()=>{
+    console.log(`O backend está rodando na porta ${port}`)
+    });
+
+    
